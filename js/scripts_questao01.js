1602d47acea6e34db32001ef21c3dba80745294b
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-11 20:07:00 -0300

    script da primeira questão

---
+++ js/scripts_questao01.js
@@ -0,0 +1,22 @@
+console.log("um texto")
+
+// PEGANDO ELEMENTOS DO DOM
+const formDados = document.querySelector('#formulario')
+const divResultado = document.querySelector('#resultado')
+
+//capturando o evento de submit
+formDados.addEventListener('submit', (evt)=> {
+    evt.preventDefault()
+
+    const form_num = new  FormData(formDados)
+
+    let num1 = parseFloat(form_num.get('num1'))
+    let num2 = parseFloat(form_num.get('num2'))
+    let num3 = parseFloat(form_num.get('num3'))
+
+    let media = parseFloat(num1 + num2 + num3) / parseFloat(3)
+    //let media = parseFloat(num1) + parseFloat(num2) + parseFloat(num3)) / parseFloat(3)
+
+    divResultado.innerHTML = `A média do s números ${media.toFixed(2).replace('.', '.')}`
+})
+
